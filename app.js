8f39e3d014fe5000c27f27b820adbdec5babb0c8
Tommy Tang <1530234656@qq.com>
2015-02-04 17:16:18 +0800

    update

--- app.js
+++ app.js
@@ -1,104 +1,32 @@
-var wechat = require("wechat");
-var express = require("express");
-var logger = require("morgan");
-var config = require("./config");
-var errorhandler = require("errorhandler");
-var bodyParser = require("body-parser");
-var fs = require("fs");
-var path = require("path");
-var os = require("os");
+var wechat          = require("wechat");
+var express         = require("express");
+var logger          = require("morgan");
+var errorhandler    = require("errorhandler");
+var bodyParser      = require("body-parser");
+var fs              = require("fs");
+var path            = require("path");
+var routes          = require("./routes");
+var config          = require("./config");
 
-var tool = require("./tool");
-var accessLogFilePath = __dirname + '/access.log';
-var errorLogFilePath = __dirname + '/error.log';
-var accessLogStream = fs.createWriteStream(accessLogFilePath, {flags: 'a'});
-var errorLogStream = fs.createWriteStream(errorLogFilePath, {flags: 'a'});
 var app = express();
 
 //config
-app.set('port', process.env.PORT || 80);
-app.use(logger('combined', {stream: accessLogStream}));
+app.set('port', process.env.PORT || config.host.port);
+app.use(logger('combined', {stream: fs.createWriteStream('./access.log', {flags: 'a'})}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use('/api/', wechat(config.wechat, function (req, res, next) {
-    // 微信输入信息都在req.weixin上
-    var message = req.weixin;
-    console.log("message\n", message);
-    var content = message.Content;
-    var msgType = message.MsgType;
-    if (msgType === 'text') {
-        if (/约/.test(content)) {
-            res.reply({
-                content: '约你妹呀！',
-                type: 'text'
-            });
-        } else if (/图/.test(content)) {
-            res.reply([
-                {
-                    title: '菇凉我的靓照!',
-                    description: '女神嫁到',
-                    picurl: 'http://picview01.baomihua.com/photos/20120119/m_14_634626046352187500_36036466.jpg',
-                    url: 'http://weibo.com/fengjieluoyufeng?c=spr_qdhz_bd_baidusmt_weibo_s&nick=%E7%BD%97%E7%8E%89%E5%87%A4'
-                }
-            ]);
-        } else if (/乐/.test(content)) {
-            res.reply({
-                type: 'music',
-                content: {
-                    title: '来段音乐吧',
-                    description: '好听的轻音乐',
-                    musicUrl: 'http://114.215.159.50:7000/%E7%BA%AF%E9%9F%B3%E4%B9%90%20-%20%E6%82%A0%E4%B9%85%E3%81%AE%E6%99%82.mp3',
-                    hqMusicUrl: 'http://114.215.159.50:7000/%E7%BA%AF%E9%9F%B3%E4%B9%90%20-%20%E6%82%A0%E4%B9%85%E3%81%AE%E6%99%82.mp3',
-                    thumbMediaId: 'thisThumbMediaId'
-                }
-            });
-        } else if (/系统/.test(content)) {
-            var attr = tool.getOsInfo();
-            var content = 'type:' + attr.type + '\r\n' + 'platform:' + attr.platform + '\r\n' + 'arch:' + attr.arch + '\r\n'
-              + 'release:' + attr.release + '\r\n' + 'hostname:' + attr.hostname;
 
-            res.reply({
-                content: content,
-                type: 'text'
-            });
+// route
+if (app.get('env') === config.host.env) {
+    app.get('/weixin', routes.weixinSetup);
+} else {
+    app.use('/weixin', routes.weixin);
+}
 
-        } else if (/内存/.test(content)) {
-            var attr = tool.getMemInfo();
-            var content = '总内存:' + attr.total + '\r\n' + '剩余内存:' + attr.free + '\r\n' + '使用率:' + attr.usePercent + '%';
-            res.reply({
-                content: content,
-                type: 'text'
-            });
-        } else {
-            res.reply({
-                content: '收到,收到, 但我不知道',
-                type: 'text'
-            });
-        }
-
-
-    } else {
-        res.reply({
-            content: '收到,收到, 但我不知道',
-            type: 'text'
-        });
-    }
-}));
-
-
-
-app.get('/api/', function (req, res) {
-    console.log('wechat connect come');
-    console.log("req.query\n", req.query);
-    res.send(req.query.echostr);
-});
-app.post('/', function (req, res) {
-    console.log("req.body\n", req.body);
-    res.send('');
-});
-
-
+// error handler
 app.use(errorhandler());
+
+// start up server
 app.listen(app.get('port'), function () {
     console.log('Server listening on:', app.get('port'));
 });
--- configDemo.js
+++ configDemo.js
@@ -3,5 +3,9 @@ module.exports = {
         token: 'tommy',
         appid: 'wx00dfe0c46246cf13',
         encodingAESKey: 'tK7jfLON3at0uTSqORQBzFU8dxrujRbt802rtJr5UEa'
+    },
+    host: {
+        port: 3001,
+        env: 'development'
     }
 };
